6502caaf110ad99d8fb4819586f07ed757ae58f1
JayeshBhosale0 <91714007+JayeshBhosale0@users.noreply.github.com>
2021-10-01 19:12:26 +0530

    Add files via upload

---
+++ Lengthofline UC2.java
@@ -0,0 +1,14 @@
+package lengthofline;
+
+public class Lengthofline {
+
+	public static void main(String[] args) {
+	     int x1,x2,y1,y2;
+		    double len;
+		    x1=2;y1=1;x2=4;y2=4;
+		    len=Math.sqrt((x2-x1)*(x2-x1) + (y2-y1)*(y2-y1));	 	    
+	            System.out.println("lengthbetween"+"("+x1+","+y1+"),"+"("+x2+","+y2+")===>"+len);
+
+	}
+
+}
---
+++ LineComputation.java
@@ -0,0 +1,7 @@
+public class LineComputation
+{
+public static void main(string[]args)
+{
+System.out.println("welcome to the line");
+}
+}
